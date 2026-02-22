0c10f0760781740922e1730f5452087410b0f1c8
Yash Chopade <chopadeyash07@gmail.com>
2026-02-22 12:10:30 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import WebsiteEditor from './pages/Editor'
 import LiveSite from './pages/LiveSite'
 import Pricing from './pages/Pricing'
 
-export const serverUrl="https://webbuilder-server.onrender.com"
+export const serverUrl="https://webbuilderai-an-website-generator.onrender.com"
 function App() {
   useGetCurrentUser()
   const {userData}=useSelector(state=>state.user)
